90f575e58178695c5cd1649694683a7b2aa4d94a
AtanasoskaBrankica <atanasoskabrankica98@gmail.com>
2022-03-09 10:58:32 +0100

    Add background black + change file layout

---
+++ src/containers/NavBar/NavBar.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export const NavBar = () => {
+  return (
+    <nav
+      className="col-3"
+      style={{
+        background: 'black',
+        height: '100vh',
+        borderRight: '1px solid white',
+      }}
+    ></nav>
+  );
+};
---
+++ src/containers/Search/Search.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export const Search = () => {
+  return (
+    <aside
+      className="col-3"
+      style={{
+        background: 'black',
+        height: '100vh',
+        borderLeft: '1px solid white',
+      }}
+    ></aside>
+  );
+};
