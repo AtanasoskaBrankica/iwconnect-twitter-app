6ffb35d5066751d1be8e11e8657de1ed2193d417
AtanasoskaBrankica <atanasoskabrankica98@gmail.com>
2022-03-09 11:25:37 +0100

    Add Main and Search component because of changes in file layout in master

---
+++ src/containers/Search/Search.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export const Search = () => {
+  return (
+    <aside
+      className="col-3"
+      style={{background: 'black', height: '100vh'}}
+    ></aside>
+  );
+};
